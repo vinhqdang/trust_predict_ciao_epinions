9b3be07705fa7a787fa198cbfed8fc61f8e14b9a
Vinh Q. DANG <dqvinh87@gmail.com>
2016-02-16 16:16:40 +0100

    Add related papers

--- code/Process.R
+++ code/Process.R
@@ -1,6 +1,7 @@
 library(R.matlab)
 library(h2o)
 library(hydroGOF)
+library(MBESS)
 
 #convert timestamp of Ciao dataset to 11 values
 convert_time_stamp = function(time_stamp) 
@@ -19,7 +20,7 @@ convert_time_stamp = function(time_stamp)
 }
 
 # time_point: TRUE if timestamp is already converted to 1..11
-rating_prediction = function(filename = "rating_with_timestamp.mat", time_point = TRUE)
+rating_prediction = function(filename = "epinions_rating_with_timestamp.mat", time_point = TRUE)
 {
   rating = readMat(filename)
   rating = rating$rating
@@ -47,26 +48,71 @@ rating_prediction = function(filename = "rating_with_timestamp.mat", time_point 
   print ("All data")
   dnn = h2o.deeplearning(x=c(1:3,5:6),y=4, training_frame = train_rating_h2o, validation_frame = test_rating_h2o)
   
-  print (sqrt(dnn@model$validation_metrics@metrics$MSE)) # 1.026661
+  rmse_value = sqrt(dnn@model$validation_metrics@metrics$MSE)
   
-  # Contain product which are already rated before
+  print (ci.rmsea(rmsea = rmse_value, df = nrow(test_rating) - 1, N = nrow(test_rating)))
+  
+  print (rmse_value) # 1.026661
+  
+  
+  # Product never been rated
   rating_new = test_rating[! (test_rating$Product %in% train_rating$Product & test_rating$User %in% train_rating$Use),]
   h2o_rate_new = as.h2o (rating_new)
   
-  dnn = h2o.deeplearning(x=c(1:3,5:6),y=4, training_frame = train_rating_h2o, validation_frame = h2o_rate_new)
-  print ("Cold start problem")
-  print (sqrt(dnn@model$validation_metrics@metrics$MSE)) # 1.006675
+#   dnn = h2o.deeplearning(x=c(1:3,5:6),y=4, training_frame = train_rating_h2o, validation_frame = h2o_rate_new)
+#   print ("Cold start problem")
+#   print (sqrt(dnn@model$validation_metrics@metrics$MSE)) # 1.006675
   
-  # Product never been rated
-  rating_old = test_rating[test_rating$Product %in% train_rating$Product,]
+  # print ("Cold start prediction")
+  p_new = h2o.predict(dnn, newdata = h2o_rate_new)
+  rmse_value = hydroGOF::rmse(as.vector(rating_new$Rating), as.vector(p_new))
+  
+  print (ci.rmsea(rmsea = rmse_value, df = nrow(rating_new) - 1, N = nrow(rating_new)))
+  print (rmse_value)
+  
+  # Contain product which are already rated before
+  rating_old = test_rating[test_rating$Product %in% train_rating$Product & test_rating$User %in% train_rating$Use,]
   h2o_rate_old = as.h2o (rating_old)
   
-  dnn = h2o.deeplearning(x=c(1:3,5:6),y=4, training_frame = train_rating_h2o, validation_frame = h2o_rate_old)
+  # dnn = h2o.deeplearning(x=c(1:3,5:6),y=4, training_frame = train_rating_h2o, validation_frame = h2o_rate_old)
   
   print ("Existing product and users rating prediction")
-  print (sqrt(dnn@model$validation_metrics@metrics$MSE)) 
+  # print (sqrt(dnn@model$validation_metrics@metrics$MSE)) 
+  p_old = h2o.predict(dnn, newdata = h2o_rate_old)
+  rmse_value = hydroGOF::rmse(as.vector(rating_old$Rating), as.vector(p_old))
+  
+  print (ci.rmsea(rmsea = rmse_value, df = nrow(rating_old) - 1, N = nrow(rating_old)))
+  print (rmse_value)
   
   h2o.shutdown(prompt = FALSE)
 }
 
-# Utility function
+trust_prediction = function (filename = "epinion_trust_with_timestamp.mat")
+{
+  h2o.init()
+  
+  trust_data = readMat(filename)
+  trust_data = as.data.frame(trust_data$trust)
+  colnames (trust_data) = c("Trustor","Trustee","Timestamp")
+  
+  trust_train = trust_data [trust_data$Timestamp != 11,]
+  trust_test = trust_data [trust_data$Timestamp == 11,]
+  
+  h2o_trust_train = as.h2o (trust_train)
+  h2o_trust_test = as.h2o (trust_test)
+  
+  dnn = h2o.deeplearning(x=1:2,y=3,training_frame = h2o_trust_train, validation_frame = h2o_trust_test)
+  
+  rating = readMat("epinions_rating_with_timestamp.mat")
+  rating = rating$rating
+  
+  rating = as.data.frame(rating)
+  
+  colnames(rating) = c("User","Product","Category","Rating","Helpfulnesss","Timestamp")
+  
+  rating$User = as.factor(rating$User)
+  # rating$V2 = as.factor(rating$V2)
+  rating$Category = as.factor(rating$Category)
+  
+  h2o.shutdown(prompt = TRUE)
+}
